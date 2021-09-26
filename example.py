bd2298da2337d49a74c9b7694c355d7f0833dd73
coneill65 <69809534+coneill65@users.noreply.github.com>
2021-09-26 14:43:29 -0500

    This is an example

---
+++ example.py
@@ -0,0 +1,20 @@
+from VARCE import CommandManager
+import pyttsx3
+
+
+engine = pyttsx3.init(driverName='sapi5')
+engine.setProperty('rate', 150)
+
+
+activation_words = ["voice assistant"]
+bot = CommandManager(activation_words)
+
+
+@bot.add_command
+def hello():
+    engine.say("I will speak this text")
+    engine.runAndWait()
+
+
+if __name__ == "__main__":
+    bot.run()
