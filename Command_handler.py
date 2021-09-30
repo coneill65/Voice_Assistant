82cda2522bbaa6c5767e05f7bec47747b8edd415
coneill65 <69809534+coneill65@users.noreply.github.com>
2021-09-30 11:29:46 -0500

    Actually recognizes voice commands!

--- Command_handler.py
+++ Command_handler.py
@@ -1,18 +1,26 @@
-import speech_recognition as sr
 import traceback
-import pyttsx3
-
-
-engine = pyttsx3.init(driverName='sapi5') 
-engine.setProperty('rate', 150)
+import speech_recognition as sr
+import threading as t
+import time
+from gtts import gTTS
+from io import BytesIO
+from pydub import AudioSegment
+from pydub.playback import play
 
 
 class BadCommandError:
     pass
 
 
+def get_available_mics(print_info=True):
+    if print_info:
+        for index, name in enumerate(sr.Microphone.list_microphone_names()):
+            print("Microphone with name \"{1}\" found for `Microphone(device_index={0})`".format(index, name))
+    return sr.Microphone.list_microphone_names()
+
+
 class CommandManager:
-    def __init__(self, activation_words, print_run_info=True, activation_sentence="What command should I run?"):
+    def __init__(self, activation_words, print_run_info=True, activation_sentence="What command should I run?", activation_text="What command would you like to run?"):
         """
         The CommandManager allows users to register functions for
         calling them in an interactive shell!
@@ -31,6 +39,8 @@ class CommandManager:
         self.print_run_info = print_run_info
         self.activation_sentence = activation_sentence
         self.online = False
+        self.activation_text = activation_text
+        self.running_command = False
 
     def run(self):
         """
@@ -39,15 +49,8 @@ class CommandManager:
 
         while True:
             try:
-                done = False
-                user_input = input("Enter command: ")
-                for word in self.activation_words:
-                    if word in user_input and done is False:
-                        word = user_input.split(word, 1)[1]
-                        self.run_command(word[1:])
-                        done = True
-                if done is False:
-                    raise "No activation Phrase was provided."
+                if self.running_command is False:
+                    self.start_thread()
             except Exception:
                 traceback.print_exc()
 
@@ -77,24 +80,74 @@ class CommandManager:
         self.commands[command.__name__] = command
         return command
 
-    def get_available_mics(self, print_info=True):
-        if print_info:
-            for index, name in enumerate(sr.Microphone.list_microphone_names()):
-                print("Microphone with name \"{1}\" found for `Microphone(device_index={0})`".format(index, name))
-        return sr.Microphone.list_microphone_names()
-
-    def get_audio(self):
+    def get_audio_online(self, timeout_var=2):
         try:
             # for testing purposes, we're just using the default API key
             # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
             # instead of `r.recognize_google(audio)`
             r = sr.Recognizer()
             with sr.Microphone() as source:
-                print("Say something!")
-                audio = r.listen(source, timeout=1, phrase_time_limit=2)
+                audio = r.listen(source, timeout=timeout_var, phrase_time_limit=2)
             audio = r.recognize_google(audio)
             return audio
         except sr.UnknownValueError:
             return None
         except sr.RequestError:
             self.online = False
+
+    def print_audio(self):
+        if self.running_command is True:
+            return
+        self.running_command = True
+        audio = str(self.get_audio_online())
+        print(audio)
+        if audio:
+            for word in self.activation_words:
+                if word in audio:
+                    audio = audio.replace(f"{word}", "", 1)
+                    audio = audio.strip()
+                    print(word, audio)
+                    if audio != "":
+                        audio = audio.replace(" ", "_")
+                        print(audio)
+                        try:
+                            self.run_command(audio)
+                        except:
+                            self.say("Say the name of the command you would like to run")
+                            listen = self.get_audio_online(timeout_var=4)
+                            print(f"listen = {listen}")
+                            if listen is not None:
+                                for command in self.commands.keys():
+                                    if str(command) in str(listen):
+                                        self.commands[command]()
+                                        break
+                    else:
+                        self.say("Say the name of the command you would like to run")
+                        listen = self.get_audio_online(timeout_var=4)
+                        print(f"listen = {listen}")
+                        for command in self.commands.keys():
+                            if command in listen:
+                                self.commands[command]()
+                                break
+                    break
+        self.running_command = False
+
+    def start_thread(self):
+        thread = t.Thread(target=self.print_audio)
+        thread.start()
+        time.sleep(1)
+
+    def say(self, text="Hello World!", lang="en"):
+
+        # get audio from server
+        tts = gTTS(text=text, lang=lang)
+
+        # convert to file-like object
+        fp = BytesIO()
+        tts.write_to_fp(fp)
+        fp.seek(0)
+
+        # --- play it ---
+
+        song = AudioSegment.from_file(fp, format="mp3")
+        play(song)
